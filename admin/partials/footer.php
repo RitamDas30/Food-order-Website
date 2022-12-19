8f146ab3ca2223250b3a31dd31d53f7c2d703662
RitamDas30 <110047613+RitamDas30@users.noreply.github.com>
2022-12-19 21:22:13 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer section Starts-->
+<div class="footer">
+        <div class="wrapper">
+        <p class="text-centre">&#169; 2022 FoodFun. All Rights Reserved by <a href="#">ARADA</a></p>
+        </div>
+    </div>
+    <!-- Footer section Ends -->
+
+
+</body>
+</html>
